86c466b83590301ae0a1395a258de1e017fee36a
Steven Da Costa <60145033+Dwarter-dev@users.noreply.github.com>
2022-02-21 13:49:44 +0100

    Ajustement de la partie Comment pour fonctionner normalement

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -6,6 +6,8 @@ use App\Entity\Comment;
 use App\Entity\Maison;
 // Classes
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+// Fonctionnement
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,7 +17,13 @@ class CommentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('comment')
+            ->add('comment', TextType::class, [
+                'required' => true, // à part
+                'label' => 'Commentaire',
+                'attr' => [
+                     'maxLenght' => 255
+                ]
+              ])
             ->add('maison', EntityType::class, [
                 'class' => Maison::class,
                 'choice_label' => 'title'
